279822e4ab43c0fe3194022cc9233492dd9cdf1c
jefersoncmn <jefersoncmn@hotmail.com>
2021-12-08 16:08:55 -0300

    feat: developers and ntf basic layout done

--- gallery/views.py
+++ gallery/views.py
@@ -1,28 +1,67 @@
 from django.shortcuts import render
 
-
 data = {
     "trucks": [
-        {"id": 0, "name": "Caminhão daora",
-         "image": "../static/images/nft_van.svg"},
-        {"id": 1, "name": "Caminhão grande",
-         "image": "../static/images/nft_van.svg"},
-        {"id": 2, "name": "Caminhão verde",
-         "image": "..../assets/images/nft_van.svg"},
-        {"id": 3, "name": "Caminhão grande",
-         "image": "../static/images/nft_van.svg"},
-        {"id": 4, "name": "Caminhão grande",
-         "image": "../static/images/nft_van.svg"},
-        {"id": 5, "name": "Caminhão grande",
-         "image": "../static/images/nft_van.svg"},
-        {"id": 6, "name": "Caminhão grande",
-         "image": "../static/images/nft_van.svg"},
-        {"id": 7, "name": "Caminhão grande",
-         "image": "../static/images/nft_van.svg"},
-        {"id": 8, "name": "Caminhão grande",
-         "image": "../static/images/nft_van.svg"},
-        {"id": 9, "name": "Caminhão grande",
-         "image": "../static/images/nft_van.svg"},
+        {
+            "id": 0,
+            "name": "Caminhão de Frutas",
+            "price": 25,
+            "cargo_type": "Frutas",
+            "cargo_limit": 24,
+            "max_velocity": 200,
+            "average_consumption": 23,
+            "comfort_level": "Médio",
+            "axle": 4,
+            "image": "../static/images/nft_van.svg"
+        },
+        {
+            "id": 1,
+            "name": "Caminhão daora",
+            "price": 25,
+            "cargo_type": "Combustível",
+            "cargo_limit": 24,
+            "max_velocity": 200,
+            "average_consumption": 23,
+            "comfort_level": "Médio",
+            "axle": 4,
+            "image": "../static/images/nft_van.svg"
+        },
+        {
+            "id": 2,
+            "name": "Caminhão daora",
+            "price": 25,
+            "cargo_type": "Frutas",
+            "cargo_limit": 24,
+            "max_velocity": 200,
+            "average_consumption": 23,
+            "comfort_level": "Médio",
+            "axle": 4,
+            "image": "../static/images/nft_van.svg"
+        },
+        {
+            "id": 3,
+            "name": "Caminhão daora",
+            "price": 25,
+            "cargo_type": "Frutas",
+            "cargo_limit": 24,
+            "max_velocity": 200,
+            "average_consumption": 23,
+            "comfort_level": "Médio",
+            "axle": 4,
+            "image": "../static/images/nft_van.svg"
+        },
+        {
+            "id": 4,
+            "name": "Caminhão daora",
+            "price": 25,
+            "cargo_type": "Frutas",
+            "cargo_limit": 24,
+            "max_velocity": 200,
+            "average_consumption": 23,
+            "comfort_level": "Médio",
+            "axle": 4,
+            "image": "../static/images/nft_van.svg"
+        },
     ]
 }
 
