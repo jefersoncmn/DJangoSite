9d7a44f1e31b31ebcfcc98d8125c4f82699d76e4
jefersoncmn <jefersoncmn@hotmail.com>
2021-12-08 14:18:38 -0300

    feat: passing nft data to other pages

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,11 +1,12 @@
 from django import urls
+from django.http import request
 from django.urls import path
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.index),
-
+    path('nft/<int:my_id>/', views.nft_view)
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- gallery/views.py
+++ gallery/views.py
@@ -1,31 +1,36 @@
 from django.shortcuts import render
 
 
-def index(request):
-    data = {
-        # "trucks_list": "Caminhão daora",
-        "trucks": [
-            {"id": 0, "name": "Caminhão daora",
-                "image": "../static/images/nft_van.svg"},
-            {"id": 1, "name": "Caminhão grande",
-                "image": "../static/images/nft_van.svg"},
-            {"id": 2, "name": "Caminhão verde",
-                "image": "..../assets/images/nft_van.svg"},
-            {"id": 3, "name": "Caminhão grande",
-                "image": "../static/images/nft_van.svg"},
-            {"id": 4, "name": "Caminhão grande",
-                "image": "../static/images/nft_van.svg"},
-            {"id": 5, "name": "Caminhão grande",
-                "image": "../static/images/nft_van.svg"},
-            {"id": 6, "name": "Caminhão grande",
-                "image": "../static/images/nft_van.svg"},
-            {"id": 7, "name": "Caminhão grande",
-                "image": "../static/images/nft_van.svg"},
-            {"id": 8, "name": "Caminhão grande",
-                "image": "../static/images/nft_van.svg"},
-            {"id": 9, "name": "Caminhão grande",
-                "image": "../static/images/nft_van.svg"},
-        ]
-    }
+data = {
+    "trucks": [
+        {"id": 0, "name": "Caminhão daora",
+         "image": "../static/images/nft_van.svg"},
+        {"id": 1, "name": "Caminhão grande",
+         "image": "../static/images/nft_van.svg"},
+        {"id": 2, "name": "Caminhão verde",
+         "image": "..../assets/images/nft_van.svg"},
+        {"id": 3, "name": "Caminhão grande",
+         "image": "../static/images/nft_van.svg"},
+        {"id": 4, "name": "Caminhão grande",
+         "image": "../static/images/nft_van.svg"},
+        {"id": 5, "name": "Caminhão grande",
+         "image": "../static/images/nft_van.svg"},
+        {"id": 6, "name": "Caminhão grande",
+         "image": "../static/images/nft_van.svg"},
+        {"id": 7, "name": "Caminhão grande",
+         "image": "../static/images/nft_van.svg"},
+        {"id": 8, "name": "Caminhão grande",
+         "image": "../static/images/nft_van.svg"},
+        {"id": 9, "name": "Caminhão grande",
+         "image": "../static/images/nft_van.svg"},
+    ]
+}
 
+
+def index(request):
     return render(request, 'gallery/index.html', data)
+
+
+def nft_view(request, my_id):
+    print(data["trucks"][my_id])
+    return render(request, 'gallery/nft.html', data["trucks"][my_id])
